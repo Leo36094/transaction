c1f8ae65d05ee43caaf60a8efcf03a9e358bc5ed
CI-Leo.Chuang <leo.chuang@cloud-interactive.com>
2019-08-12 02:46:01 +0800

    Updates

---
+++ precache-manifest.9e8c95457ac407a98c8c79befe1b818e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b504d42191f659cf517a0e7a05b327",
+    "url": "/transaction-procedure/index.html"
+  },
+  {
+    "revision": "91e79aaf631c9b0b14b4",
+    "url": "/transaction-procedure/static/css/main.5436f773.chunk.css"
+  },
+  {
+    "revision": "563394b5966103e4c64e",
+    "url": "/transaction-procedure/static/js/2.c37676ba.chunk.js"
+  },
+  {
+    "revision": "91e79aaf631c9b0b14b4",
+    "url": "/transaction-procedure/static/js/main.f4bfb230.chunk.js"
+  },
+  {
+    "revision": "e5d8ac3956093909a5e6",
+    "url": "/transaction-procedure/static/js/runtime~main.c3c3c178.js"
+  },
+  {
+    "revision": "ca068ac6eada8d3215fadc06a90cb48f",
+    "url": "/transaction-procedure/static/media/product@2x.ca068ac6.png"
+  }
+]);
